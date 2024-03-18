4d8a93064dde7d1aa51a2144b443a7ae48726ab8
Juan Nunez-Iglesias <jni@fastmail.com>
2024-03-18 19:48:10 +1100

    Make message a multiline string

--- src/annotrack/annotation.py
+++ src/annotrack/annotation.py
@@ -144,25 +144,26 @@ class SampleViewer:
         self.v = viewer
         self.v_source = 'self'
         if viewer is not None:
-            m = 'Mark samples as correct or incorrect by pressing y or n, \n'
-            m = m + 'repspectively. \n'
-            m = m + '---------------------------------------------------------\n'
-            m = m + 'To indicate that an ID swap has has occured, at a given \n'
-            m = m + 'point in time press i.\n'
-            m = m + '---------------------------------------------------------\n'
-            m = m + 'To indicate that a false termination or start has occured\n'
-            m = m + 'at a given point in time, use t or Shift-t, respectively.\n'
-            m = m + '---------------------------------------------------------\n'
-            m = m + 'To indicate that a tracking error at a given time point\n'
-            m = m + 'is associated with a segmentation error, press s.\n'
-            m = m + '---------------------------------------------------------\n'
-            m = m + 'Pressing i, t or Shift-t will record the frame number at\n'
-            m = m + 'which the error occured. This will be saved into the \n'
-            m = m + 'info csv in the sample.smpl directory\n'
-            m = m + '---------------------------------------------------------\n'
-            m = m + 'To navagate to the next sample, use 2. To move to the \n'
-            m = m + 'previous sample, use 1. (a keyboard agnostic approach)\n'
-            m = m + '---------------------------------------------------------\n'
+            message = (
+            """Mark samples as correct or incorrect by pressing y or n,
+            respectively.
+            
+            To indicate that an ID swap has occurred, navigate to the first
+            frame of the new track and press i.
+            
+            To indicate that a false termination or start has occurred,
+            navigate to the frame where it occurs and press t or Shift-t.
+            
+            To indicate that a tracking error (any of the above) is caused by
+            a segmentation error, press s.
+            
+            These error types will be saved to the info.csv file in the
+            sample.smpl directory.
+            
+            To navigate to the next sample, use 2, and use 1 to navigate to the
+            previous sample.
+            """
+            )
             print(m)
             self.v_source = 'preexising'
 
