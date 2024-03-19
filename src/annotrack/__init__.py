2705ba604940b1f19e3230ac4f8ddd7da821960f
Juan Nunez-Iglesias <jni@fastmail.com>
2024-03-19 17:31:26 +1100

    Update version to v0.0.3rc0

--- src/annotrack/__init__.py
+++ src/annotrack/__init__.py
@@ -2,6 +2,8 @@ from .sampling import sample_tracks, sample_track_terminations, get_sample_hyper
 from .annotation import SampleViewer
 from .sample_management import prepare_sample_for_annotation
 
+__version__ = '0.0.3rc0'
+
 __all__ = [
     'sample_tracks', 
     'sample_track_terminations', 
